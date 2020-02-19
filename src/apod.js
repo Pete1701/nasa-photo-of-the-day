24ca682845a47a4bd9101415dddd253ef2d89b19
Pete1701 <pete.petrongonas@pirron.one>
2020-02-20 00:17:57 +0100

    MVP

---
+++ src/apod.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+//import ApodCard from "./apodCard";
+
+function Apod() { 
+    const [obj, setObj] = useState([]);
+
+    useEffect(() => {
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+        .then(response => {
+            console.log(response.data);
+            setObj(response.data);
+        })
+        .catch(error => {
+            console.log('No Data', error);
+        });
+    }, []);
+    return (
+        <div>
+            <h3>{obj.title}</h3>
+            <h5>{obj.date}</h5>
+            <p>{obj.explanation}</p>
+            <img src={obj.url} alt='Space image'/>        
+        </div>
+    );
+}
+
+export default Apod;
